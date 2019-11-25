b683570672dadc4a71bb01f79f8f973ae412ce63
sexytoaster <paddyoconnor1@hotmail.com>
2019-11-25 19:31:41 +0000

    added in audiovisual stuff from class
    Added in audio stuff we did in class the other week to have a starting point to work from. Played around with a few variables to refresh knowledge

---
+++ GE1Assignment/Assets/Scripts/audioAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioAnalyzer : MonoBehaviour
+{
+    public int frameSize = 512;
+    public static float[] samples;
+    public static float[] spectrum;
+    AudioSource audio;
+    // Awake is called before everything
+    void Awake()
+    {
+        //raw data we're reading in
+        samples = new float[frameSize];
+        //wave height
+        spectrum = new float[frameSize];
+        //audio
+        audio = GetComponent<AudioSource>();
+        //start playback
+        audio.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //getting samples from the audio
+        audio.GetOutputData(samples, 0);
+        //get spectrum data
+        audio.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+    }
+}
---
+++ GE1Assignment/Assets/Scripts/audioVisualizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioVisualizer : MonoBehaviour
+{
+    public float radius = 30;
+    public float scale = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float theta = (Mathf.PI * 2.0f) / audioAnalyzer.spectrum.Length;
+        //create a cube for each frame in the sample
+        for(int i = 0; i < audioAnalyzer.spectrum.Length; i++)
+        {
+            //instanciate cube
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            float angle = theta * i;
+            Vector3 p = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
+            cube.transform.position = transform.TransformPoint(p);
+            cube.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.up);
+            cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float)audioAnalyzer.spectrum.Length, 1, 1);
+            cube.transform.parent = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < audioAnalyzer.spectrum.Length; i++)
+        {
+            transform.GetChild(i).localScale = new Vector3(1, (1 + audioAnalyzer.spectrum[i] * scale), 1);
+        }
+        
+    }
+}
