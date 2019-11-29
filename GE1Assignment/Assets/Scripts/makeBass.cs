08a587f1bee4a12926db82f7daf73ce108452c98
sexytoaster <paddyoconnor1@hotmail.com>
2019-11-29 19:58:59 +0000

    instanciating in the bass rings
    made a little script that basically just spawns in the rings based on the cameras position, so as the camera moves forward more rings will be added. It just does this along the x axis. Infinte tunnel kind of amirite?

---
+++ GE1Assignment/Assets/Scripts/makeBass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class makeBass : MonoBehaviour
+{
+    public GameObject ring;
+    public GameObject mainCamera;
+    public Vector3 lastPosition = new Vector3(0, 0, 0);
+    public Vector3 offsetVector = new Vector3(10, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(mainCamera.transform.position, lastPosition);
+        if (distance < 100)
+        {
+            GameObject ringInstance = GameObject.Instantiate(ring, lastPosition + offsetVector, Quaternion.identity);
+            lastPosition = ringInstance.transform.position;
+        }
+    }
+}
