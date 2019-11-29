8c573e08b388309e025117d1b793c09545152b5e
sexytoaster <paddyoconnor1@hotmail.com>
2019-11-29 19:41:14 +0000

    experimenting with bass shapes
    As my idea is centred around a tunnel, i think the tunnel could be made of rings that respond to the bass. So I added in a ring that responds to bass

---
+++ GE1Assignment/Assets/Scripts/bassRingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bassRingScript : MonoBehaviour
+{
+    public int band;
+    public float startingScale, scaleMult;
+    public bool useBuffer;
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().materials[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer.audioBandBuffer[band] * scaleMult) + startingScale, (AudioPeer.audioBandBuffer[band] * scaleMult) + startingScale);
+            Color color = new Color(AudioPeer.audioBandBuffer[band], AudioPeer.audioBandBuffer[band], AudioPeer.audioBandBuffer[band]);
+            material.SetColor("_EmissionColor", color);
+        }
+        if (!useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer.bands[band] * scaleMult) + startingScale , (AudioPeer.bands[band] * scaleMult) + startingScale);
+            Color color = new Color(AudioPeer.audioBand[band], AudioPeer.audioBand[band], AudioPeer.audioBand[band]);
+            material.SetColor("_EmissionColor", color);
+        }
+
+
+    }
+}
