ec6fbc535cad806281f97cd7fef3e108ee4c1f9b
sexytoaster <paddyoconnor1@hotmail.com>
2019-11-29 23:33:48 +0000

    Skybox Subbase
    A few new additions, destroying objects we're passing now and  I made the skybox flash with the subbase. looking lit.

---
+++ GE1Assignment/Assets/Scripts/SkyboxBass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxBass : MonoBehaviour
+{
+    public int band;
+    public float duration;
+    //gonna take rgb values from a sample materiaal and use them to effect the time of the skybox. I want top keep the skybox white because it'll be easier to test stuff
+    public Material sampleMaterial;
+    float r, g, b;
+    //offset is to help reduce the brightness of the tint so its not so hard to look at
+    public float offset = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color colourOriginal = sampleMaterial.GetColor("_EmissionColor");
+        r = (1 - colourOriginal.r);
+        g = (1 - colourOriginal.g);
+        b = (1 - colourOriginal.b);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color color = new Color(AudioPeer.audioBandBuffer[band] - (r + offset), AudioPeer.audioBandBuffer[band] - (g + offset), AudioPeer.audioBandBuffer[band] - (b + offset));
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        RenderSettings.skybox.SetColor("_Tint", Color.Lerp(Color.black, color, lerp));
+
+    }
+}
---
+++ GE1Assignment/Assets/Scripts/cameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public float speed = 50.0f;
+    public int band;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = this.speed;
+        transform.position += mainCamera.transform.forward * speed * Time.deltaTime * (AudioPeer.audioBand[band] + 1);
+    }
+}
